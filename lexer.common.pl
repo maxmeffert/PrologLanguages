11bef069f9f1797dbb4919e814f6e001aa091c20
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2019-08-26 15:39:56 +0200

    renaming and formatting, made it explicit that these method expect codes as input

--- lexer.common.pl
+++ lexer.common.pl
@@ -1,29 +1,66 @@
 % ==============================================================================
+%
 % Character/Code Recognition Utils
+%
 % ==============================================================================
 
-space --> [S], { code_type(S, space) }.
-space(S) --> [S], { code_type(S, space) }.
-spaces([S|SS]) --> space(S), spaces(SS).
+% ------------------------------------------------------------------------------
+% Whitespaces
+% ------------------------------------------------------------------------------
+
+whitespace(Code) :- code_type(Code, space).
+notWhitespace(Code) :- \+ code_type(Code, space).
+
+space --> [Code], { whitespace(Code) }.
+space(Code) --> [Code], { whitespace(Code) }.
+
+spaces([Code|Codes]) --> space(Code), spaces(Codes).
 spaces([]) --> \+ space(_).
 
-notspace(S) --> [S], { \+ code_type(S,space) }.
-notspaces([S|SS]) --> notspace(S), notspaces(SS).
+notspace(Code) --> [Code], { notWhitespace(Code) }.
+notspaces([Code|Codes]) --> notspace(Code), notspaces(Codes).
 notspaces([]) --> \+ notspace(_).
 
-digit(D) --> [D], { code_type(D, digit) }.
-digits([D|DS]) --> digit(D), digits(DS).
+% ------------------------------------------------------------------------------
+% Digits
+% ------------------------------------------------------------------------------
+
+digit(Code) :- code_type(Code,digit).
+
+digit(Code) --> [Code], { digit(Code) }.
+
+digits([Code|Codes]) --> digit(Code), digits(Codes).
 digits([]) --> \+ digit(_).
 
-letter(L) --> [L], { code_type(L, alpha) }.
-letters([L|LS]) --> letter(L), letters(LS).
+% ------------------------------------------------------------------------------
+% Letters
+% ------------------------------------------------------------------------------
+
+letter(Code) :- code_type(Code,alpha).
+
+letter(Code) --> [Code], { letter(Code) }.
+
+letters([Code|Codes]) --> letter(Code), letters(Codes).
 letters([]) --> \+ letter(_).
 
-letterOrDigit(X) --> [X], { code_type(X,digit) ; code_type(X,alpha) }.
-lettersOrDigits([X|XS]) --> letterOrDigit(X), lettersOrDigits(XS).
+% ------------------------------------------------------------------------------
+% Letters or Digits
+% ------------------------------------------------------------------------------
+
+letterOrDigit(Code) :- letter(Code) ; digit(Code).
+
+letterOrDigit(Code) --> [Code], { letterOrDigit(Code) }.
+
+lettersOrDigits([Code|Codes]) --> letterOrDigit(Code), lettersOrDigits(Codes).
 lettersOrDigits([]) --> \+ letterOrDigit(_).
+
+
+% ------------------------------------------------------------------------------
+% String-Code Comparison
+% ------------------------------------------------------------------------------
 
 code(String,Code) --> [Code], { string_codes(String,[Code]) }.
 codes(String,Codes) --> Codes, { string_codes(String,Codes) }.
+
 notCode(String,Code) --> [Code], { string_codes(String,[C]), \+ C = Code }.
 notCodes(String,Codes) --> Codes, { string_codes(String,CS), \+ CS = Codes }.
