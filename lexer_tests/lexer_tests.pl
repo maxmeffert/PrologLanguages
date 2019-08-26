7eae71fdc80a720c27dd8a1d5c46bac120b9c8af
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2019-08-26 20:28:20 +0200

    adjusted paths

--- lexer_tests/lexer_tests.pl
+++ lexer_tests/lexer_tests.pl
@@ -1,8 +1,8 @@
-:- ['lexer.tests.whitespace.pl'].
-:- ['lexer.tests.boolean.pl'].
-:- ['lexer.tests.integer.pl'].
-:- ['lexer.tests.float.pl'].
-:- ['lexer.tests.string.pl'].
-:- ['lexer.tests.keyword.pl'].
-:- ['lexer.tests.misc.pl'].
-:- ['lexer.tests.lexer_from_file.pl'].
+:- ['lexer_tests_whitespaces.pl'].
+:- ['lexer_tests_booleans.pl'].
+:- ['lexer_tests_integers.pl'].
+:- ['lexer_tests_floats.pl'].
+:- ['lexer_tests_strings.pl'].
+:- ['lexer_tests_keywords.pl'].
+:- ['lexer_tests_misc.pl'].
+:- ['lexer_tests_lexer_from_file.pl'].
--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
-swipl -s lexer.tests/lexer.tests.all.pl -t run_tests
-swipl -s lexer.tests/lexer.common.tests.pl -t run_tests
+swipl -s lexer_tests/lexer_tests.pl -t run_tests
+# swipl -s lexer.tests/lexer.common.tests.pl -t run_tests
