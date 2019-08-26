6d6b0417c7524ebc249a6207503f3ef0a6dc4471
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2019-08-26 20:03:01 +0200

    adjusted file path

--- lexer.tests/lexer.tests.lexer_from_file.pl
+++ lexer.tests/lexer.tests.lexer_from_file.pl
@@ -2,7 +2,7 @@
 :- ensure_loaded('../lexer/lexer.pl').
 
 test(lexer_from_file) :- 
-    lexer_from_file("lexer_test_file.txt",Tokens),
+    lexer_from_file("./lexer.tests/lexer_test_file.txt",Tokens),
     assertion(Tokens=[token(keyword,while),token(keyword,if),token(string,"hello world.")]).
     
 :- end_tests(lexer_from_file).
