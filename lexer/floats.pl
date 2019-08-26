11d8fc251022391504d23bd8cd67fb7eec67d5ef
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2019-08-26 22:16:51 +0200

    renaming

--- lexer/floats.pl
+++ lexer/floats.pl
@@ -10,25 +10,25 @@
 :- use_module(lexer/digits).
 :- use_module(lexer/integers).
 
-floatDecimalPoint(Code) --> string_code_dcg(".",Code).
-floatExponent(Code) --> string_code_dcg("e",Code).
+float_decimal_point_dcg(Code) --> string_code_dcg(".",Code).
+float_exponent_dcg(Code) --> string_code_dcg("e",Code).
 
-floatFractionLiteral([P,D|DS]) --> floatDecimalPoint(P), digit_dcg(D), digits_dcg(DS).
+float_fraction_literal_dcg([P,D|DS]) --> float_decimal_point_dcg(P), digit_dcg(D), digits_dcg(DS).
 
-floatExponentLiteral([FloatExponent|IntegerLiteral]) --> floatExponent(FloatExponent), signedIntegerLiteral(IntegerLiteral).
-floatExponentLiteral([FloatExponent|IntegerLiteral]) --> floatExponent(FloatExponent), integerLiteral(IntegerLiteral).
+float_exponent_literal_dcg([FloatExponent|IntegerLiteral]) --> float_exponent_dcg(FloatExponent), signedIntegerLiteral(IntegerLiteral).
+float_exponent_literal_dcg([FloatExponent|IntegerLiteral]) --> float_exponent_dcg(FloatExponent), integerLiteral(IntegerLiteral).
 
-floatLiteral(FloatLiteral) --> integerLiteral(IntegerLiteral), floatFractionLiteral(FloatFractionLiteral), floatExponentLiteral(FloatExponentLiteral), {
+float_literal_dcg(FloatLiteral) --> integerLiteral(IntegerLiteral), float_fraction_literal_dcg(FloatFractionLiteral), float_exponent_literal_dcg(FloatExponentLiteral), {
     append(IntegerLiteral,FloatFractionLiteral,X),
     append(X,FloatExponentLiteral,FloatLiteral)
 }.
-floatLiteral(FloatLiteral) --> integerLiteral(IntegerLiteral), floatFractionLiteral(FloatFractionLiteral), {
+float_literal_dcg(FloatLiteral) --> integerLiteral(IntegerLiteral), float_fraction_literal_dcg(FloatFractionLiteral), {
     append(IntegerLiteral,FloatFractionLiteral,FloatLiteral)
 }.
-floatLiteral(FloatLiteral) --> integerLiteral(IntegerLiteral), floatExponentLiteral(FloatExponentLiteral), {
+float_literal_dcg(FloatLiteral) --> integerLiteral(IntegerLiteral), float_exponent_literal_dcg(FloatExponentLiteral), {
     append(IntegerLiteral,FloatExponentLiteral,FloatLiteral)
 }.
 
-token_float_dcg(FloatValue) --> floatLiteral(FloatLiteral), {
+token_float_dcg(FloatValue) --> float_literal_dcg(FloatLiteral), {
     number_string(FloatValue,FloatLiteral)
 }.
